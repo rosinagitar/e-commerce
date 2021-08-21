65344a4724f056e72a0d4116351c68bb9aaf20d1
rosinagitar <rosinagitar98@gmail.com>
2021-08-21 19:16:14 -0300

    entrega 1
    cambios en products, login y home

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+/*document.addEventListener("DOMContentLoaded", function(e){
 
-});
+});*/
+
+function checkUsernameAndPassword(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if((username !=="") && (password !=="")){
+        window.location.href="home.html";
+    } else {
+        alert("Debe completar ambos campos");
+    }
+}
